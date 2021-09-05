7d1ff8e68f91f07637ad3115b5a3a2068e91a79f
Jeremymcg88 <jeremy.mcguirk@gmail.com>
2021-09-05 12:53:59 -0400

    added assets folder and CSS folder. Linked bootstrap and font-awesome. constructed first API call to return temp, long/lat.

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+
 // creating on click for search btn
 let searchBtn = document.getElementById('search');
+let cityInput = "";
+let clearInput = "";
+let cityName = "";
 
+
+// returns zip code information after the click happens 
 searchBtn.addEventListener('click', function() {
     console.log(document.getElementById('location').value)
 
+// API key that returns the zipcode typed in by user 
    var url = `https://api.openweathermap.org/data/2.5/weather?q=${document.getElementById('location').value}&appid=bd0834857d11c7c26292f5e1e8657635&units=imperial`
 
    console.log('Url ????', url)
 
-
+ // Always want to convert fetch responses to Json for user readability 
    fetch(url)
    .then(function(response) {
        console.log('RAW RESPONSE from over the inter!!', response)
@@ -28,7 +35,7 @@ function oneCallApi(lat, lon) {
     console.log('lat and lon we gave to our new fucntin thing!', lat, lon)
     var oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=bd0834857d11c7c26292f5e1e8657635&units=imperial`
 
-    console.log('Awesome new url!! that we are gonna smack!', oneCallUrl)
+    console.log('Awesome new url!! that gives lon/lat', oneCallUrl)
 
     fetch(oneCallUrl)
    .then(function(response) {
@@ -36,8 +43,8 @@ function oneCallApi(lat, lon) {
        return response.json()
     })
    .then(function(data) {
-        console.log('WHOLE DATA  ONE CALL API thing THING!!!!', data)    
-        // put that stuff on the page!!
+        console.log('WHOLE DATA  ONE CALL API!!!!', data)    
+        // this is the information we want displayed on page 
 
    });
 }
